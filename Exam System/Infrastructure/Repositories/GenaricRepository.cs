eb788db4de26f3264f4332bc14855710f7f4bca6
Administrator <Administrator@DESKTOP-854KJI4>
2025-09-25 14:50:52 +0300

    Add Gnaricrepository

---
+++ Exam System/Infrastructure/Repositories/GenaricRepository.cs
@@ -0,0 +1,32 @@
+﻿using Exam_System.Shared.Interface;
+
+namespace Exam_System.Infrastructure.Repositories
+{
+    public class GenaricRepository : IGenericRepository
+    {
+        public Task<T> AddAsync<T>(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync<T>(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> GetByIdAsync<T>(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync<T>(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
