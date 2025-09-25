d85b0c007968bfbcda3c25cc4046909c02c8af30
Shaimaa S Soliman <shaimaassiliman@gmail.com>
2025-09-25 14:40:09 +0300

    add Generic Repositroy

---
+++ Exam System/Shared/Interface/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Exam_System.Shared.Interface
+{
+    public interface IGenericRepository
+    {
+        Task<T> AddAsync<T>(T entity) where T : class;
+        Task<T> UpdateAsync<T>(T entity) where T : class;
+        Task DeleteAsync<T>(T entity) where T : class;
+        Task<T?> GetByIdAsync<T>(Guid id) where T : class;
+        Task<IEnumerable<T>> GetAllAsync<T>() where T : class;
+
+    }
+}
