fe57e486cc337ca4f0e50db9bd2fc702b655fe48
Youssefabdulaty <youssef.ys665@gmail.com>
2025-09-30 00:32:42 +0300

    Create RegisterUser Feature

---
+++ Exam System/Feature/User/RegisterUser/RegisterCommandHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace Exam_System.Feature.User.RegisterUser
+{
+    public class RegisterCommandHandler
+    {
+    }
+}
---
+++ Exam System/Feature/User/RegisterUser/RegisterEndPoint.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exam_System.Feature.User.RegisterUser
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public UserController(IMediator _mediator)
+        {
+            mediator = _mediator;
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterDTO regiserDTO) { 
+            return Ok();
+        }
+    }
+}
