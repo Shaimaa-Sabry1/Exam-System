420ae2305317a3f92c91f6e8285c4b29acd3380b
Administrator <Administrator@DESKTOP-854KJI4>
2025-09-30 21:08:21 +0300

    Getallrefactor

---
+++ Exam System/Feature/Queries/GetAllExams/GetAllExamEndpoint.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exam_System.Feature.Queries.GetAllExams
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExamController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllExams([FromQuery] GetAllExamQuery query, [FromServices] IMediator mediator)
+        {
+            var result = await mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
